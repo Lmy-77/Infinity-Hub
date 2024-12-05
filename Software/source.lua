9600575c0f1d148130ce0db2bbac65f082f9bc81
Lmy <lmadeira657@gmail.com>
2024-12-05 11:49:13 -0300

    Update source.lua

--- Software/source.lua
+++ Software/source.lua
@@ -1,1 +1,10 @@
-print('Soon')
+local games = {
+    [{16732694052}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/fish/sorce.lua'
+}
+
+for ids, url in next, games do
+    if table.find(ids, game.PlaceId) then
+        loadstring(game:HttpGet(url))()
+        break
+    end
+end
