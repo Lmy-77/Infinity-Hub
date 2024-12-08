67441fdc92b52d422b034309c4e6278b1960049e
Lmy77 <lmadeira657@gmail.com>
2024-12-08 20:41:49 -0300

    Update sorce.lua

--- games/fish/sorce.lua
+++ games/fish/sorce.lua
@@ -156,7 +156,7 @@ local Toggle = FischTab:CreateToggle({
       game.Players.LocalPlayer.PlayerGui.DescendantAdded:Connect(function(Descendant)
          if autoshake then
              if Descendant.Name == 'button' and Descendant.Parent.Name == 'safezone' then
-                 task.wait(0.26)
+                 task.wait(0.24)
                  game:GetService('GuiService').SelectedObject = Descendant
                  vim:SendKeyEvent(true, Enum.KeyCode.Return, false, game)
                  vim:SendKeyEvent(false, Enum.KeyCode.Return, false, game)
