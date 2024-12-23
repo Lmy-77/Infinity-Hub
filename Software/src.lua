02be0894b8b4c60864313a7d39256e5d176ed491
Lmy77 <lmadeira657@gmail.com>
2024-12-23 14:41:23 -0300

    Update src.lua

--- Software/src.lua
+++ Software/src.lua
@@ -1,7 +1,7 @@
 local games = {
     [{16732694052}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/fish/src.lua',
     [{5610197459}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/GangUpOnPeopleSimulator/src.lua',
-    [{893973440}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/FleeTheFacility/src.lua',
+    [{893973440}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/FleeTheFacility/src.lua'
 }
 
 for ids, url in next, games do
