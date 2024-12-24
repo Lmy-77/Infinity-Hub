09a246c762b11e222fdae403c9cd6a4d2a45c390
Lmy77 <lmadeira657@gmail.com>
2024-12-23 23:01:22 -0300

    Update src.lua

--- games/FleeTheFacility/Mobile/Button/src.lua
+++ games/FleeTheFacility/Mobile/Button/src.lua
@@ -80,28 +80,6 @@ local function NMBE_fake_script()
 end
 coroutine.wrap(NMBE_fake_script)()
 local function XHCTHDF_fake_script()
-    local UIS = game:GetService("UserInputService")
-    local frame = script.Parent
-    local dragging = false
-    local dragStart
-    local startPos
-    frame.InputBegan:Connect(function(input)
-        if input.UserInputType == Enum.UserInputType.MouseButton1 then
-            dragging = true
-            dragStart = input.Position
-            startPos = frame.Position
-        end
-    end)
-    frame.InputChanged:Connect(function(input)
-        if dragging and input.UserInputType == Enum.UserInputType.MouseMovement then
-            local delta = input.Position - dragStart
-            frame.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
-        end
-    end)
-    UIS.InputEnded:Connect(function(input)
-        if input.UserInputType == Enum.UserInputType.MouseButton1 then
-            dragging = false
-        end
-    end)
+	print('a')
 end
 coroutine.wrap(XHCTHDF_fake_script)()
