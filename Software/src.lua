4d5e4df4e0e612f58d5dec1f3631d24a8bf33e78
Lmy77 <lmadeira657@gmail.com>
2024-12-27 00:38:10 -0300

    Update src.lua

--- Software/src.lua
+++ Software/src.lua
@@ -3,6 +3,7 @@ local games = {
     [{5610197459}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/GangUpOnPeopleSimulator/src.lua',
     [{893973440}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/FleeTheFacility/src.lua',
     [{5780309044}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/Stands%20Awakening/Main/src.lua',
+    [{11423467063}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/Stands%20Awakening/Main/autoboss.lua'
 }
 
 for ids, url in next, games do
