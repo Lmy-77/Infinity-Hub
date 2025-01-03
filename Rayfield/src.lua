8f9dc07599aac7da86c0ea7ae29d0cafba62e125
Lmy77 <lmadeira657@gmail.com>
2025-01-03 15:43:56 -0300

    Update src.lua

--- Rayfield/src.lua
+++ Rayfield/src.lua
@@ -19,7 +19,7 @@ local ConfigurationExtension = ".rfld"
 local settingsTable = {
 	General = {
 		-- if needs be in order just make getSetting(name)
-		rayfieldOpen = {Type = 'bind', Value = 'K', Name = 'Rayfield Keybind'},
+		rayfieldOpen = {Type = 'bind', Value = 'K', Name = 'Infinity Hub Keybind'},
 		-- buildwarnings
 		-- rayfieldprompts
 
