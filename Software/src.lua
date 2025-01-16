a319712ab5ae30ed63594b7d99c8f97b336b1f0e
Lmy77 <lmadeira657@gmail.com>
2025-01-16 14:15:02 -0300

    Update src.lua

--- Software/src.lua
+++ Software/src.lua
@@ -17,7 +17,8 @@ local games = {
     [{5780309044}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/Stands%20Awakening/Main/src.lua',
     [{11423467063}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/Stands%20Awakening/Main/autoboss.lua',
     [{10450270085, 16379688837}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/Jujutsu%20Infinite/src.lua',
-    [{10260193230}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/Meme%20Sea/src.lua'
+    [{10260193230}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/Meme%20Sea/src.lua',
+    [{95284069025576}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/FishRNG/scr.lua'
 }
 for ids, url in next, games do
     if table.find(ids, game.PlaceId) then
