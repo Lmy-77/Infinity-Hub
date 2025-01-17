5a05e86b4a4229ea78ad36410f392f71c942dae1
Lmy77 <lmadeira657@gmail.com>
2025-01-16 21:30:50 -0300

    Update src.lua

--- Software/src.lua
+++ Software/src.lua
@@ -1,5 +1,4 @@
 -- teleport service
-local found = false
 local queue_on_teleport = queue_on_teleport or syn and syn.queue_on_teleport
 queue_on_teleport[[
     repeat wait() until game:IsLoaded() print("ServerHoped or rejoined")
@@ -10,7 +9,8 @@ queue_on_teleport[[
 
 
 -- loader
-local games = {
+local currentPlaceId = game.PlaceId
+local supportedGames = { 
     [{16732694052}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/fish/src.lua',
     [{5610197459}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/GangUpOnPeopleSimulator/src.lua',
     [{893973440}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/FleeTheFacility/src.lua',
@@ -18,15 +18,27 @@ local games = {
     [{11423467063}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/Stands%20Awakening/Main/autoboss.lua',
     [{10450270085, 16379688837}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/Jujutsu%20Infinite/src.lua',
     [{10260193230}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/Meme%20Sea/src.lua',
-    [{95284069025576}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/FishRNG/scr.lua'
+    [{95284069025576}] = 'https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/scripts/games/FishRNG/scr.lua',
 }
-for ids, url in next, games do
-    if table.find(ids, game.PlaceId) then
-        loadstring(game:HttpGet(url))()
-        found = true
-        break
+local function findScript()
+    for key, scriptUrl in pairs(supportedGames) do
+        if typeof(key) == "table" then
+            for _, id in ipairs(key) do
+                if id == currentPlaceId then
+                    return scriptUrl
+                end
+            end
+        elseif key == currentPlaceId then
+            return scriptUrl
+        end
     end
+    return nil
 end
-if not found then
+local scriptUrl = findScript()
+if scriptUrl then
+    print("Infinity Hub - Game supported")
+    loadstring(game:HttpGet(scriptUrl))()
+else
+    print("Infinity Hub - Game not supported")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Lmy-77/Infinity-Hub/refs/heads/modules/Notification/Support.lua"))()
 end
