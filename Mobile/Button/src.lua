55369e183684fa622c009345280fcebfd3854423
Lmy77 <lmadeira657@gmail.com>
2025-01-26 11:49:02 -0300

    Update src.lua

--- Mobile/Button/src.lua
+++ Mobile/Button/src.lua
@@ -1,85 +1,127 @@
-local Mobile = Instance.new("ScreenGui")
-local Button = Instance.new("Frame")
+local MobileButton = Instance.new("ScreenGui")
+local Main = Instance.new("TextButton")
 local UICorner = Instance.new("UICorner")
-local Icon = Instance.new("ImageButton")
-local UIStroke = Instance.new("UIStroke")
 local UIGradient = Instance.new("UIGradient")
+local UIStroke = Instance.new("UIStroke")
 local UIGradient_2 = Instance.new("UIGradient")
-local Interact = Instance.new("TextButton")
+local Icon = Instance.new("ImageLabel")
+local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
+local UIAspectRatioConstraint_2 = Instance.new("UIAspectRatioConstraint")
 
 
+MobileButton.Name = "Mobile Button"
+MobileButton.Parent = (game:GetService("CoreGui") or gethui())
+MobileButton.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-Mobile.Name = "Mobile"
-Mobile.Parent = (game:GetService("CoreGui") or gethui())
-Mobile.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+Main.Name = "Main"
+Main.Parent = MobileButton
+Main.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Main.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Main.BorderSizePixel = 0
+Main.Position = UDim2.new(0.0429799445, 0, 0.117917307, 0)
+Main.Size = UDim2.new(0.0429799445, 0, 0.0612557419, 0)
+Main.AutoButtonColor = false
+Main.Font = Enum.Font.SourceSans
+Main.Text = ""
+Main.TextColor3 = Color3.fromRGB(0, 0, 0)
+Main.TextSize = 14.000
 
-Button.Name = "Button"
-Button.Parent = Mobile
-Button.AnchorPoint = Vector2.new(0.5, 0.5)
-Button.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Button.BorderColor3 = Color3.fromRGB(27, 42, 53)
-Button.BorderSizePixel = 0
-Button.LayoutOrder = 6
-Button.Position = UDim2.new(0.0955775008, 0, 0.138591111, 0)
-Button.Size = UDim2.new(0, 42, 0, 40)
-Button.ZIndex = 150
+UICorner.CornerRadius = UDim.new(1, 0)
+UICorner.Parent = Main
 
-UICorner.Parent = Button
+UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(95, 101, 106)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(54, 60, 61))}
+UIGradient.Rotation = 50
+UIGradient.Parent = Main
 
-Icon.Name = "Icon"
-Icon.Parent = Button
-Icon.AnchorPoint = Vector2.new(0.5, 0.5)
-Icon.BackgroundTransparency = 1.000
-Icon.BorderColor3 = Color3.fromRGB(27, 42, 53)
-Icon.BorderSizePixel = 0
-Icon.LayoutOrder = 1
-Icon.Position = UDim2.new(0.5, 0, 0.5, 0)
-Icon.Size = UDim2.new(0, 24, 0, 24)
-Icon.ZIndex = 150
-Icon.Image = "rbxassetid://10723415766"
-Icon.ImageColor3 = Color3.fromRGB(240, 240, 240)
-Icon.ScaleType = Enum.ScaleType.Fit
-
+UIStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
 UIStroke.Color = Color3.fromRGB(255, 255, 255)
 UIStroke.Thickness = 1.200
-UIStroke.Parent = Button
+UIStroke.Parent = Main
 
-UIGradient.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(81, 87, 91)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(44, 49, 50))}
-UIGradient.Rotation = 50
-UIGradient.Parent = UIStroke
-
-UIGradient_2.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(88, 94, 99)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(38, 42, 43))}
+UIGradient_2.Color = ColorSequence.new{ColorSequenceKeypoint.new(0.00, Color3.fromRGB(81, 87, 91)), ColorSequenceKeypoint.new(1.00, Color3.fromRGB(44, 49, 50))}
 UIGradient_2.Rotation = 50
-UIGradient_2.Parent = Button
+UIGradient_2.Parent = UIStroke
 
-Interact.Name = "Interact"
-Interact.Parent = Button
-Interact.AnchorPoint = Vector2.new(0.5, 0.5)
-Interact.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Interact.BackgroundTransparency = 1.000
-Interact.BorderColor3 = Color3.fromRGB(27, 42, 53)
-Interact.BorderSizePixel = 0
-Interact.Position = UDim2.new(0.5, 0, 0.5, 0)
-Interact.Size = UDim2.new(1, 0, 1, 0)
-Interact.ZIndex = 155
-Interact.Font = Enum.Font.SourceSans
-Interact.Text = ""
-Interact.TextColor3 = Color3.fromRGB(0, 0, 0)
-Interact.TextSize = 14.000
-Interact.TextTransparency = 1.000
+Icon.Name = "Icon"
+Icon.Parent = Main
+Icon.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Icon.BackgroundTransparency = 1.000
+Icon.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Icon.BorderSizePixel = 0
+Icon.Position = UDim2.new(0.166966751, 0, 0.214989409, -1)
+Icon.Size = UDim2.new(0.666666687, 0, 0.590909064, 0)
+Icon.Image = "rbxassetid://10723415766"
+Icon.ImageTransparency = 0.500
+
+UIAspectRatioConstraint.Parent = Icon
+UIAspectRatioConstraint.AspectRatio = 1.154
+
+UIAspectRatioConstraint_2.Parent = Main
+UIAspectRatioConstraint_2.AspectRatio = 1.023
 
 
-
-local function NMBE_fake_script()
-	local script = Instance.new('LocalScript', Interact)
+local function SZBOR_fake_script()
+	local script = Instance.new('LocalScript', Main)
 
 	local button = script.Parent
+	local guiPath = game:GetService('CoreGui')
 	button.MouseButton1Click:Connect(function()
-		game:GetService('VirtualInputManager'):SendKeyEvent(true, "K", false, game)
+		for _, v in pairs(guiPath:GetDescendants()) do
+			if v:IsA('ScreenGui') and v.Name == 'FluentRenewed_Infinity Hub - 2.8a | Flee The Facility' then
+				if v.Enabled == true then
+					v.Enabled = false
+				elseif v.Enabled == false then
+					v.Enabled = true
+				end
+			end
+		end
 	end)
 end
-coroutine.wrap(NMBE_fake_script)()
-local function XHCTHDF_fake_script()
-	print('a')
+coroutine.wrap(SZBOR_fake_script)()
+local function CAGKXJ_fake_script()
+	local script = Instance.new('LocalScript', Main)
+
+	local frame = script.Parent
+	local userInputService = game:GetService("UserInputService")
+	
+	local dragging = false
+	local dragStart, startPos
+	
+	local function update(input)
+		local delta = input.Position - dragStart
+		frame.Position = UDim2.new(
+			startPos.X.Scale,
+			startPos.X.Offset + delta.X,
+			startPos.Y.Scale,
+			startPos.Y.Offset + delta.Y
+		)
+	end
+	
+	frame.InputBegan:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+			dragging = true
+			dragStart = input.Position
+			startPos = frame.Position
+	
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragging = false
+				end
+			end)
+		end
+	end)
+	
+	frame.InputChanged:Connect(function(input)
+		if dragging and (input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch) then
+			update(input)
+		end
+	end)
+	
+	userInputService.InputChanged:Connect(function(input)
+		if dragging and (input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch) then
+			update(input)
+		end
+	end)
+	
 end
-coroutine.wrap(XHCTHDF_fake_script)()
+coroutine.wrap(CAGKXJ_fake_script)()
